40003d76783a73f518de9b4d917fd408d9ab66dc
SpineSociety <Kokakisan@gmail.com>
2026-09-22 16:07:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -145,7 +145,11 @@ window.showClubRoomTab = function (tabName) {
   }
 };
 document.getElementById("nookTab").addEventListener("click", () => showScreen("nook"));
-document.getElementById("libraryTab").addEventListener("click", () => showScreen("collection"));
+const libraryTab = document.getElementById("libraryTab");
+
+if (libraryTab) {
+  libraryTab.addEventListener("click", () => showScreen("collection"));
+}
 document.getElementById("randomizerTab").addEventListener("click", () => showScreen("randomizer"));
 document.getElementById("stacksTab").addEventListener("click", () => showScreen("stacks"));
 document.getElementById("reflectionTab").addEventListener("click", () => showScreen("reflection"));
